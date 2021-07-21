17974d4189c709e897ba6a8f596cf4b627d738ed
Amanuel2000 <amanuelf44@gmail.com>
2021-07-21 20:52:49 +0300

    another commit

--- employeemanager/src/main/java/tech/getarrys/employeemanager/repo/EmployeeRepo.java
+++ employeemanager/src/main/java/tech/getarrys/employeemanager/repo/EmployeeRepo.java
@@ -1,2 +1,10 @@
-package tech.getarrys.employeemanager.repo;public interface EmployeeRepo {
+package tech.getarrys.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrys.employeemanager.model.Employee;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Employee findEmployeeById(Long id);
 }
